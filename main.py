af5ea521105b42d12ebfcd786c3815dbecc5128b
Ezio <assassinste@gmail.com>
2019-12-18 22:48:47 +0800

    Named parameters as sample data

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ conn_status(conn)
 cursor_status(cur)
 
 TABLE_NAME = "table1st_basic_usage"
-SAMPLE_DATA = (1, "ONEONE")
+SAMPLE_DATA = {"num": 1, "data": "ONEONE"}
 
 
 stmt_create_table = (
     "CREATE TABLE {table} "
     "(id serial PRIMARY KEY, num integer, data varchar);"
 )
-stmt_insert_record = "INSERT INTO {table} (num, data) " "VALUES (%s, %s);"
+stmt_insert_record = (
+    "INSERT INTO {table} (num, data) " "VALUES (%(num)s, %(data)s);"
+)
 stmt_select_all = "SELECT * FROM {table};"
 
 cur.execute(
